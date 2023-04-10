7e7549f556ac9ddcb0f8557fcd5ea650d0c74169
Elena Kvasko <lenakvasko@gmail.com>
2023-04-11 00:37:29 +0300

    Update files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { nanoid } from 'nanoid';
 import React from 'react';
 import { Component } from 'react';
 import { ContactForm } from './ContactForm';
+import { ContactList } from './ContactList';
 
 export class App extends Component {
   state = {
@@ -14,20 +15,44 @@ export class App extends Component {
     filter: '',
   };
 
-  formSubmitHandler = data => {
-    const { name, number } = data;
+  addContact = ({ name, number }) => {
     const newContact = {
       id: nanoid(),
       name,
       number,
     };
+    this.state.contacts.filter(
+      contact =>
+        contact.name.toLowerCase().trim() ===
+          newContact.name.toLowerCase().trim() ||
+        contact.number.trim() === newContact.number.trim()
+    ).length
+      ? alert(`${newContact.name}: is already in contacts`)
+      : this.setState(({ contacts }) => {
+          return {
+            contacts: [newContact, ...contacts],
+          };
+        });
+  };
 
-    this.setState(({ contacts }) => ({
-      contacts: [newContact, ...contacts],
-    }));
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== contactId
+        ),
+      };
+    });
   };
 
   render() {
-    return <ContactForm onSubmit={this.formSubmitHandler} />;
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        <ContactList deleteContact={this.deleteContact} />
+      </>
+    );
   }
 }
---
+++ src/components/ContactList.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const ContactList = ({ contacts, deleteContact }) => {
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id}>
+          <span>{name}:</span>
+          <span>{number}</span>
+          <button type="button" onClick={() => deleteContact(id)}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ),
+  deleteContact: PropTypes.func.isRequired,
+};
